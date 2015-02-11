cdc1077e12852f8c46e5d0c3c76e453926db733f
Tim Dorscheidt <tim@thehyve.nl>
2015-02-11 16:33:10 +0100

    FT-1256 Improve clarity of Fisher output file Cleaner column names. And made compatible with changed column names. Also made names for end-result statistics more human readable.

--- web-app/Rscripts/TableWithFisher/FisherTableLoader.R
+++ web-app/Rscripts/TableWithFisher/FisherTableLoader.R
@@ -28,12 +28,14 @@ FisherTable.loader <- function(
  {
  	######################################################
 	#Read the line graph data.
-	line.data<-read.delim(input.filename,header=T)
+	line.data <- read.delim(input.filename, header = T, row.names = 1)
+	cleanHeaderNames <- gsub("\"", "", strsplit(readLines(input.filename, n = 1),' *\t *')[[1]])
+	colnames(line.data) <- cleanHeaderNames[-1]
 	######################################################
-	
-	if(length(unique(line.data$X)) < 2) stop("||FRIENDLY||The Fisher test requires at least two groups for each variable. The intersection of the groups you selected for the independent variable with the data available in the dependent variable yielded only one group with a none zero number of subjects in the independent variable. Please verify your input and try again.")
-	if(length(unique(line.data$Y)) < 2) stop("||FRIENDLY||The Fisher test requires at least two groups for each variable. The intersection of the groups you selected for the dependant variable with the data available in the independent variable yielded only one group with a none zero number of subjects in the dependant variable. Please verify your input and try again.")
-	
+
+	if(length(unique(line.data[ , 1])) < 2) stop("||FRIENDLY||The Fisher test requires at least two groups for each variable. The intersection of the groups you selected for the independent variable with the data available in the dependent variable yielded only one group with a none zero number of subjects in the independent variable. Please verify your input and try again.")
+	if(length(unique(line.data[ , 2])) < 2) stop("||FRIENDLY||The Fisher test requires at least two groups for each variable. The intersection of the groups you selected for the dependant variable with the data available in the independent variable yielded only one group with a none zero number of subjects in the dependant variable. Please verify your input and try again.")
+
 	######################################################
 	if(("GROUP" %in% colnames(line.data)) && ("GROUP.1" %in% colnames(line.data)))
 	{
@@ -45,24 +47,24 @@ FisherTable.loader <- function(
 		{
 			#Build a list of indexes which represent the records we need to pull for each group.
 			currentIndex <- which(line.data$GROUP==currentGroup)
-				
+
 			#Pull the records into another object.
 			currentGroupingData <- line.data[currentIndex,]
-			
+
 			trimmedGroupName <- gsub("^\\s+|\\s+$", "",currentGroup)
-			
+
 			#Run the lm function on each grouping.
 			lapply(split(currentGroupingData,currentGroupingData$GROUP.1),FisherTable.loader.single,"GROUP.1",trimmedGroupName)
 		}
-		
+
 		#This calls the first function on each "GROUP" which will call another function on "GROUP.1"
-		lapply(groupList,subFunctionForFisher,groupedData)	
+		lapply(groupList,subFunctionForFisher,groupedData)
 	}
-	else if("GROUP.1" %in% colnames(line.data)) 
+	else if("GROUP.1" %in% colnames(line.data))
 	{
 		lapply(split(line.data, line.data$GROUP.1), FisherTable.loader.single,"GROUP.1","")
 	}
-	else if("GROUP" %in% colnames(line.data)) 
+	else if("GROUP" %in% colnames(line.data))
 	{
 		lapply(split(line.data, line.data$GROUP), FisherTable.loader.single,"GROUP","")
 	}
@@ -70,7 +72,7 @@ FisherTable.loader <- function(
 	{
 		FisherTable.loader.single(line.data,'','')
 	}
-	
+
 	######################################################
 }
 
@@ -78,29 +80,29 @@ FisherTable.loader.single <- function(dataChunk,splitColumn,fileNameQualifier)
 {
 	#This is the name of the output file for the statistical tests.
 	statisticalTestsResultsFile <- paste("statisticalTests",fileNameQualifier,".txt",sep="")
-	
+
 	#This is the name of the output file for the count table.
 	countsFile <- paste("Count",fileNameQualifier,".txt",sep="")
 
 	#This is the current group we are generating the statistics for.
 	currentGroup <- unique(dataChunk[[splitColumn]])
-	
+
 	#If we have a group column we should write the group name to the file.
 	if(splitColumn %in% colnames(dataChunk)) write(paste("name=",currentGroup,sep=""), file=statisticalTestsResultsFile,append=T)
 	if(splitColumn %in% colnames(dataChunk)) write(paste("name=",currentGroup,sep=""), file=countsFile,append=T)
-	
+
 	#Remove unwanted column.
-	dataChunk <- dataChunk[c('X','Y')]
-	
+	dataChunk <- dataChunk[ , c(1, 2)]
+
 	#Recreate the factors to take out the levels we don't use in this group.
-	dataChunk$X <- factor(dataChunk$X)
-	dataChunk$Y <- factor(dataChunk$Y)
-	
+	dataChunk[ , 1] <- factor(dataChunk[ , 1])
+	dataChunk[ , 2] <- factor(dataChunk[ , 2])
+
 	#Generate count table.
 	countTable <- table(dataChunk)
 
 	#Do not continue with statistical tests if data requirements are not met
-    if (nlevels(dataChunk$X) < 2 || nlevels(dataChunk$Y) < 2) {
+    if (nlevels(dataChunk[ , 1]) < 2 || nlevels(dataChunk[ , 2]) < 2) {
       write(paste("Not enough levels for Fisher or chi-square test."), file=statisticalTestsResultsFile,append=T)
       write.table(countTable,countsFile,quote=F,sep="\t",row.names=T,col.names=T,append=T)
       return()
@@ -108,15 +110,15 @@ FisherTable.loader.single <- function(dataChunk,splitColumn,fileNameQualifier)
 
 	#Get fisher test statistics.
 	fisherResults <- fisher.test(countTable,simulate.p.value=TRUE)
-		
+
 	#Get chi^2 test statistics.
-	chiResults <- chisq.test(countTable)	
-	
+	chiResults <- chisq.test(countTable)
+
 	#Write the results of the tests to a file.
-	write(paste("fishp=",format(fisherResults$p.value,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
-	write(paste("chis=",format(chiResults$statistic,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
-	write(paste("chip=",format(chiResults$p.value,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
-	
+	write(paste("Fisher exact test p-value = ",format(fisherResults$p.value,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
+	write(paste("Chi-squared test statistic = ",format(chiResults$statistic,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
+	write(paste("Chi-squared p-value = ",format(chiResults$p.value,digits=3),sep=""), file=statisticalTestsResultsFile,append=T)
+
 	#Print count table to file.
 	write.table(countTable,countsFile,quote=F,sep="\t",row.names=T,col.names=T,append=T)
 }
